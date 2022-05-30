47619def677a3ad876373f9bdf9a742d13d39457
Ana Carolina Bizinoto <acbsnutri@gmail.com>
2022-05-30 14:41:49 -0300

    html page updates with data entry space for project progress

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function carregar () {
-  
+function calcular () {
+   /* var TMB Mulher = 655 + (9,6 * P) + (1,8 * A) – (4,7 * I) */
+   /* var TMB Homem = 66 + (13,7 * P) + (5 * A) – (6,8 * I) */
+
 }
